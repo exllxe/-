8f253d2d98a3b49abda5e89ec01ca4366d0f10b3
exllxe <a17562543@gmail.com>
2021-06-09 10:12:02 +0800

    0516HW

---
+++ 0513/index.js
@@ -0,0 +1,17 @@
+//import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './App.js'
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+reportWebVitals();
